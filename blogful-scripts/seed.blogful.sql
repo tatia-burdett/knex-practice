05ba4f72f567efeac1d9b2ff6386211d4d001a27
Tatia burdett <tatiaburdett@gmail.com>
2021-03-10 16:22:32 -0700

    write tests for blogful

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,22 @@
+INSERT INTO blogful_articles (title, content, date_published)
+VALUES
+  ('Article 1', 'Sample content text', now() - '21 days'::INTERVAL),
+  ('Article 2', 'Sample content text for article 2', now() - '21 days'::INTERVAL),
+  ('Article 3', 'Sample content text for article 3', now() - '21 days'::INTERVAL),
+  ('Article 4', 'Sample content text inputed here', now() - '21 days'::INTERVAL),
+  ('Article 5', 'Sample content text lalala', now() - '21 days'::INTERVAL),
+  ('Article 6', 'Sample content text something different', now() - '21 days'::INTERVAL),
+  ('Article 7', 'Sample content text look mountains', now() - '21 days'::INTERVAL),
+  ('Article 8', 'Sample content text pheonix', now() - '21 days'::INTERVAL),
+  ('Article 9', 'Sample content text los angelas', now() - '21 days'::INTERVAL),
+  ('Article 10', 'Sample content text sacramento', now() - '21 days'::INTERVAL),
+  ('Article 11', 'Sample content text san franiscio', now() - '21 days'::INTERVAL),
+  ('Article 12', 'Sample content text portland', now() - '21 days'::INTERVAL),
+  ('Article 13', 'Sample content text seattle', now() - '21 days'::INTERVAL),
+  ('Article 14', 'Sample content text wish for', now() - '21 days'::INTERVAL),
+  ('Article 15', 'Sample content text create more', now() - '21 days'::INTERVAL),
+  ('Article 16', 'Sample content text live more', now() - '21 days'::INTERVAL),
+  ('Article 17', 'Sample content text live happily', now() - '21 days'::INTERVAL),
+  ('Article 18', 'Sample content text lacking ideas', now() - '21 days'::INTERVAL),
+  ('Article 19', 'Sample content text near the finish', now() - '21 days'::INTERVAL),
+  ('Article 20', 'Sample content text bam', now() - '21 days'::INTERVAL);
